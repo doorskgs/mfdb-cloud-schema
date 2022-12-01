23e89c47b74a947e4621af95f056d8c7cb9252f9
oboforty <rajmund.csombordi@gmail.com>
2022-12-01 03:16:25 +0100

    Static website service for S3 static hosting

---
+++ cloudapp/debug_static.py
@@ -0,0 +1,19 @@
+from eme.static import EmeStaticWebsite
+
+from project import project
+
+
+if __name__ == "__main__":
+    import sys
+
+    try:
+        _, env = sys.argv
+    except:
+        env = 'dev'
+
+    project.debug_builders = True
+
+    with project.app_builder('FaaS:static_app', env=env) as app_builder:
+        app = app_builder.build_app(env=env)
+
+    app.run(host=app.host, port=app.port, threaded=False, debug=True)
